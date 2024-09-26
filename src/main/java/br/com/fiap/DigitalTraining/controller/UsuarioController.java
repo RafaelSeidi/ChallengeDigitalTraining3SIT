731e995ff8c1f0cada7934ce46ef787dd177886f
RafaelSeidi <rafasuzukitomita@gmail.com>
2024-09-26 20:08:58 -0300

    API de listagem de usuário

--- src/main/java/br/com/fiap/DigitalTraining/controller/UsuarioController.java
+++ src/main/java/br/com/fiap/DigitalTraining/controller/UsuarioController.java
@@ -1,10 +1,12 @@
 package br.com.fiap.DigitalTraining.controller;
 
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,4 +54,10 @@ public class UsuarioController {
 	public Usuario cadastrar(@RequestBody Usuario user) {
 		return userService.saveUsuario(user);
 	}
+	
+	@GetMapping("/usuario/listar")
+	public List<Usuario> listar() {
+		return userService.listaUsuario();
+	}
+	
 }
--- src/main/java/br/com/fiap/DigitalTraining/service/UsuarioService.java
+++ src/main/java/br/com/fiap/DigitalTraining/service/UsuarioService.java
@@ -1,5 +1,7 @@
 package br.com.fiap.DigitalTraining.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +25,10 @@ public class UsuarioService {
 	
 	public Usuario getUserById(Long usuarioId) {
 		return repository.findById(usuarioId).orElse(null);
+	}
+	
+	public List<Usuario> listaUsuario() {
+		return repository.findAll();
 	}
 	
 }
